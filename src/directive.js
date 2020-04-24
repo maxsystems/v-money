14a72b1e1d21194435a8c04996855e57d5b0e5ac
Albert Martin <albert@imor.tl>
2020-04-24 14:47:55 -0500

    chore: use spread instead of custom assign utility

--- src/directive.js
+++ src/directive.js
@@ -1,10 +1,9 @@
 import {format, setCursor, event} from './utils'
-import assign from './assign'
 import defaults from './options'
 
 export default function (el, binding) {
   if (!binding.value) return
-  var opt = assign(defaults, binding.value)
+  var opt = { ...defaults, ...binding.value }
 
   // v-money used on a component that's not a input
   if (el.tagName.toLocaleUpperCase() !== 'INPUT') {
